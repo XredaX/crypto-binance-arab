6f65ce3f83130ccd9925748e60861d097f88b6c1
Reda El Bettioui <74994440+XredaX@users.noreply.github.com>
2022-08-27 08:44:24 -0700

    Update database.py

--- database.py
+++ database.py
@@ -26,33 +26,8 @@ class user():
             post1 = {"msg":'^bot%'}
             new = {"$set":{"message_id":message_id}}
             collection.update_one(post1, new)
-
-    def findwords(collection, Owenr, target):
-        collection = db[collection]
-        words = {"Owenr":Owenr, "target":target}
-        data = collection.find_one(words)
-        countwords = collection.count_documents(words)
-        return data, countwords
         
     def addsession(collection, message_id, msg):
         collection = db[collection]
         newsession = {"message_id":message_id, "msg":msg}
         collection.insert_one(newsession)
-
-    def findpost(collection, Owenr, share):
-        collection = db[collection]
-        post = {"Owenr":Owenr, "share":share}
-        data = collection.find(post)
-        countposts = collection.count_documents(post)
-        return data, countposts
-
-    def addpost(collection, Owenr, share, post):
-        collection = db[collection]
-        newspost = {"Owenr":Owenr, "share":share, "post":post}
-        collection.insert_one(newspost)
-
-    def editpost(collection, Owenr, share, post):
-        collection = db[collection]
-        post1 = {"Owenr":Owenr, "share":share}
-        new = {"$set":{"post":post}}
-        collection.update_one(post1, new)
